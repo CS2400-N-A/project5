cad5dd8e5e2bd904e045190ed3dbbff4aecb8dad
rainyday21 <rainydayscholar21@gmail.com>
2021-11-28 00:47:43 -0800

    started DriverGT class

---
+++ DriverGT.java
@@ -0,0 +1,19 @@
+public class DriverGT{
+	public static void main(String[] args) {
+		DirectedGraph<Character> graph = new GraphInterFace<Character>(); // creates new graph
+		
+		char[] vertices = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
+		String[] edges = {"(A,B)", "(A, D)", "(A, E)", "(B, E)", "(D, G)", "(E, F)", "(E, H)", "(G, H)", "(F, C)", "(F,H)", "(H, I)", "(C, B)", "(I, F)"};
+		
+		for (int i = 0; i < vertices.length; i++) {
+			if (i == 0) {
+				graph = new DirectedGraph<Character>('A'); //sets A as root
+			}
+			else {
+				graph.add(vertices[i]); // adds rest of vertices to the graph
+			}
+		}
+		
+		
+	}
+}
