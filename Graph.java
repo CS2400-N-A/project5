aacbaf4b37e43ca0f14b09d11ae0f91f8eaf1a82
rainyday21 <rainydayscholar21@gmail.com>
2021-11-29 05:51:57 -0800

    alternate graph.java file with slide source code

---
+++ Graph.java
@@ -0,0 +1,37 @@
+// Taken from professor's slides, 
+
+public class Graph<T>
+{
+	private boolean[][] edges;
+	private E[] labels;
+	
+	public Graph() {
+		this(null);
+	}
+	
+	public Graph(int n) {
+		edges = new boolean[n][n];
+		labels = (E[]) new Object[n];
+	}
+	
+	// Remove an edge
+    public void removeEdge(int source, int target) {
+        edges[source][target] = false;
+    }
+    // Change the label of a vertex of this Graph
+    public void setLabel(int vertex, T newLabel) {
+        labels[vertex] = newLabel;
+    }
+    // Accessor method to determine the number of vertices in this Graph
+    public int size() {
+        return labels.length;
+    }
+    
+    public QueueInterface<T> getBreadthFirstTraversal(T originLabel) {
+        recetVertices();
+        QueueInterface<T> traversalOrder = new LinkedQueue<T>();
+        QueueInterface<GraphVertex<T>> vertex Queue = new LinkedQueue<GraphVertex<T>>();
+    }
+    
+    
+}
---
+++ GraphVertex.java
@@ -0,0 +1,53 @@
+class GraphVertex<T> {
+	private T data;
+	private GraphVertex<T> predecessor;
+	private boolean visited;
+	private boolean hasNeighbor = false;
+	private int numberofNeighbors;
+	
+	public GraphVertex() {
+		this(null);
+	}
+	
+	public GraphVertex(T dat) {
+		data = dat;
+		
+	}
+	
+	
+	public T getData() {
+		return data;
+	}
+	
+	public void setData(T input) {
+		data = input;
+	}
+	
+	public boolean isVisited() {
+		return visited;
+	}
+	
+	public void setVisted(boolean visit) {
+		visited = visit;
+	}
+	
+	public boolean hasNeighbor() {
+		return hasNeighbor;
+	}
+	
+	public boolean connect(GraphVertex<T> nextvertex) {
+		boolean b = false;
+		if (nextvertex != null) {
+			numberofNeighbors++;
+			hasNeighbor = b = true;
+			nextvertex.setPredecessor(this);
+		}
+		return b;
+	}
+	
+	public void setPredecessor(GraphVertex<T> pre) {
+		predecessor = pre;
+	}
+	
+	
+}
