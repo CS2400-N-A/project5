cdf9bdd56061282791a636e817e3890aab986877
Carlos Castrillo <cjcastrillo@cpp.edu>
2021-12-01 00:18:29 -0800

    Fixed compiler errors in driver

--- DriverGT.java
+++ DriverGT.java
@@ -1,23 +1,16 @@
 public class DriverGT{
 	public static void main(String[] args) {
-		DirectedGraph<Character> graph = new GraphInterFace<Character>(); // creates new graph
 		
 		char[] vertices = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
-		String[] edges = {"(A,B)", "(A, D)", "(A, E)", "(B, E)", "(D, G)", "(E, F)", "(E, H)", "(G, H)", "(F, C)", "(F,H)", "(H, I)", "(C, B)", "(I, F)"};
-		
-		for (int i = 0; i < vertices.length; i++) {
-			if (i == 0) {
-				graph = new DirectedGraph<Character>('A'); //sets A as root
-			}
-			else {
-				graph.add(vertices[i]); // adds rest of vertices to the graph
-			}
+		/**Defines these edges
+		 * (A,B), (A, D), (A, E), (B, E), (D, G), (E, F), (E, H), 
+		 * (G, H) (F, C), (F,H), (H, I), (C, B), (I, F)
+		 * A=0, B=1, C=2, ... I=8
+		 */
+		int[] edges = {0, 1, 0, 3, 0, 4, 1, 4, 3, 6, 4, 5, 4, 7, 6, 7, 5, 7, 7, 8, 2, 1, 8, 5};
+		Graph<Character> graph = new Graph<Character>(vertices.length); // declears a new graph
+		for(int i=0; i<edges.length-2; i+=2){	//Creates the edges
+			graph.addEdge(i, i+1);
 		}
-		
-		/* for (String e: edges) {
-			graph.connect(e.charAt(1), e.charAt(3));
-		} */
-		
-		
 	}
 }
