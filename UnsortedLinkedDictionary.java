7c274f8b78b73bff5164e40d11c3dc1ea05ac05a
rainyday21 <rainydayscholar21@gmail.com>
2021-12-01 23:30:33 -0800

    get iterator methods set.

--- UnsortedLinkedDictionary.java
+++ UnsortedLinkedDictionary.java
@@ -2,133 +2,173 @@ import java.util.Iterator;
 
 public class UnsortedLinkedDictionary<K, V> implements DictionaryInterface<K, V>
 {
-private Node firstNode;   // Reference to first node of chain
-private int  numberOfEntries; 
+    private Node firstNode;   // Reference to first node of chain
+    private int  numberOfEntries; 
 
-public UnsortedLinkedDictionary()
-{
-	firstNode = null;
-	numberOfEntries = 0;
+public UnsortedLinkedDictionary(){
+    firstNode = null;
+    numberOfEntries = 0;
 } // end default constructor
 
-		   /* < Implementations of methods in DictionaryInterface. >  . . . */
- 
-      private class Node
-      {
-private K key;
-private V value;
-private Node next;
-
-private Node(K searchKey, V dataValue)
-{
-key = searchKey;
-value = dataValue;
-next = null;
-} // end constructor
-
-private Node(K searchKey, V dataValue, Node nextNode)
-{
-key = searchKey;
-value = dataValue;
-next = nextNode;
-} // end constructor
-} // end Node
-
-    @Override
     public V add(K key, V value) {
-        
-        Node newNode = new Node(key, value);
-        newNode.next = firstNode;
-        firstNode = newNode;
-        numberOfEntries++;
-    }
-
-    @Override
-    public V remove(K key) {
-        // TODO Auto-generated method stub
-        if (firstNode != null) {
-            V value = firstNode.getValue(key);
-            firstNode = firstNode.next;
-            numberOfEntries--;
-            return value;
+        if(key == null || value == null)
+            throw new IllegalArgumentException("No null entries");
+        else{
+            V item = null;
+            Node currentNode = firstNode;
+            while(currentNode != null){
+                if(currentNode.key.equals(key)){
+                    item = currentNode.value;
+                    currentNode.value = value;
+                    return item;
+                }
+                currentNode = currentNode.next;
+            }
+            if(isEmpty()){
+                Node newNode = new Node(key, value);
+                firstNode = newNode;
+            }
+            else{
+                Node newNode = new Node(key, value, firstNode);
+                firstNode = newNode;
+            }
+            numberOfEntries++;
+            return item;
         }
-        return null;
     }
 
-    @Override
-    public V getValue(K key) {
-        // TODO Auto-generated method stub
-       do {
-        
-       } while
-        
-        
+    public V remove(K key) {
+        V item = null;
+        Node currentNode = firstNode;
+        while(currentNode != null){
+            if(currentNode.key.equals(key)){
+                item = currentNode.value;
+                numberOfEntries--;
+                currentNode.key = firstNode.key;
+                currentNode.value = firstNode.value;
+                firstNode = firstNode.next;
+                return item;
+            }
+            currentNode = currentNode.next;
+        }
+        return item;
     }
 
-    @Override
+    public V getValue(K key){
+        V value = null;
+        Node currentNode = firstNode;
+        while(currentNode != null){
+            if(currentNode.key.equals(key)){
+                value = currentNode.value;
+                return value;
+            }
+            currentNode = currentNode.next;
+        }
+        return value;
+    }
+
     public boolean contains(K key) {
-        // TODO Auto-generated method stub
-        return false;
+        boolean found = false;
+        Node currentNode = firstNode;
+        if(currentNode == null)
+            return found;
+        while(currentNode != null){
+            if(currentNode.key.equals(key)){
+                found = true;
+                return found;
+            }
+            currentNode = currentNode.next;
+        }
+        return found;
     }
 
     @Override
     public Iterator<K> getKeyIterator() {
-        // TODO Auto-generated method stub
-        K[] temp = new K[numberOfEntries];
-        while (firstNode != null) {
-            
-        }
-        Iterator<K> iter  = 
-        return iter;
-        
+        Iterator<K> keyI = new KeyIterator<K>();
+        return keyI;
     }
 
     @Override
     public Iterator<V> getValueIterator() {
-        // TODO Auto-generated method stub
-        return null;
+        Iterator<V> keyV = new KeyIterator<V>();
+        return keyV;
     }
 
-    @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        return false;
+        return numberOfEntries == 0 && firstNode == null;
     }
 
-    @Override
     public int getSize() {
-        // TODO Auto-generated method stub
         return numberOfEntries;
     }
 
-    @Override
     public void clear() {
-        // TODO Auto-generated method stub
-        
+        firstNode = null;
+        numberOfEntries = 0;
     }
+ 
+      private class Node{
+        private K key;
+        private V value;
+        private Node next;
+
+        private Node(K searchKey, V dataValue){
+            key = searchKey;
+            value = dataValue;
+            next = null;
+            } // end constructor
+
+        private Node(K searchKey, V dataValue, Node nextNode){
+            key = searchKey;
+            value = dataValue;
+            next = nextNode;
+            } // end constructor
+        } // end Node
+			private class KeyIterator<K> implements Iterator<K> {
     
-    private class NodeIterator<E> implements Iterator<E> {
-	
-	
-		public boolean hasNext() {
-			if(firstNode == null || firstNode.next == null) {
-				return false;
+    
+				public boolean hasNext() {
+					if(firstNode == null || firstNode.next == null) {
+						return false;
+					}
+					return true;
+				}
+				
+				public K next() {
+					Node nextNode = firstNode.next;
+					
+				return nextNode.getValue();
+				}
+				
+				public void remove() {
+					
+				}
+			
 			}
-			return true;
-		}
-		
-		public E next() {
-			Node nextNode = firstNode.next;
 			
-		return nextNode.getValue();
-		}
-		
-		public void remove() {
+			private class ValueIterator<V> implements Iterator<V> {
+    
+                private K key;
+				public boolean hasNext() {
+					if(firstNode == null || firstNode.next == null) {
+						return false;
+					}
+					return true;
+				}
+				
+				public V next() {
+					Node nextNode = firstNode.next;
+					
+				return nextNode.getValue(key);
+				}
+				
+				public void remove() {
+					
+				}
+				
+				
 			
-		}
-	
-	}
-	
+			}
+			
+		
 } // end UnsortedLinkedDictionary
-
-
